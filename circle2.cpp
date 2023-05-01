320260cf6b18e5b6c857853ace08b3e3e8212269
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-05-01 10:42:12 +0600

    C++ circle input radious & total area size

---
+++ circle2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  cout << "Enter a radius: ";
+  double radius;
+  cin >> radius;
+  const double pi = 3.14;
+double pow(double base, double exponent);
+double area = pi * pow(radius, 2);
+  cout << area;
+  return 0;
+}
